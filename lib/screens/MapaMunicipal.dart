5ea07c59df318bc28c7137804df2e6675822cf20
Tobias Dornelles <tobiasdornelles@hotmail.com>
2022-05-25 13:09:29 -0300

    Mapa Municipal Criado
    sem polylines implementados

--- lib/screens/MapaMunicipal.dart
+++ lib/screens/MapaMunicipal.dart
@@ -1,18 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:davidzonecidadao/main.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+
 
 class MapaMunicipal extends StatelessWidget {
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(-22.906546036007246, -47.04614884200783);
+
+  MapaMunicipal({Key? key}) : super(key: key);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  // TODO: colocar a setinha de retorno na appbar
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(56),
-        child: CustomAppBar(),
-      ),
-      body: Center(
-        child: Text(
-            'Mapa Municipal'
+    return MaterialApp(
+      home: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(56),
+          child: CustomAppBar(),
+        ),
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.7,
+          ),
         ),
       ),
     );
