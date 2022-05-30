e5a0af7efbf1057ed9392d5ecd3223f0317d086e
David Megumi Shinjo <dvdshinjo@gmail.com>
2022-05-30 13:38:58 -0300

    Forms CidadaoInfo

--- lib/screens/ComprarTicket.dart
+++ lib/screens/ComprarTicket.dart
@@ -35,8 +35,100 @@ class ComprarTicket extends StatelessWidget {
   }
 }
 
-class bottomContainerComprarTicket extends StatelessWidget {
-  const bottomContainerComprarTicket({Key? key}) : super(key: key);
+class bottomContainerComprarTicket extends StatefulWidget {
+  @override
+  _bottomContainerComprarTicket createState() {
+    return new _bottomContainerComprarTicket();
+  }
+}
+
+class _bottomContainerComprarTicket
+    extends State<bottomContainerComprarTicket> {
+  // const bottomContainerComprarTicket({Key? key}) : super(key: key);
+
+  final _plate = TextEditingController();
+  final _id = TextEditingController();
+  bool _validateId = false;
+  bool _validatePlate = false;
+
+  @override
+  void dispose() {
+    _plate.dispose();
+    _id.dispose();
+    super.dispose();
+  }
+
+  int verifyPlate(String plate) {
+    const letters = 3;
+    const nums = 4;
+
+    plate = plate.toUpperCase();
+    // Check the length
+    if (plate.length != (letters + nums)) {
+      return 1;
+    }
+    // Check the letters
+    for (int l = 0; l < letters; l++) {
+      if (!plate[l].contains(new RegExp(r'[A-Z]'))) {
+        return 2;
+      }
+    }
+    // Check the numbers
+    for (int n = letters; n < (letters + nums); n++) {
+      if (!plate[n].contains(new RegExp(r'[0-9]'))) {
+        return 3;
+      }
+    }
+    return 0;
+  }
+
+  String getPlateMessage(int plateCode) {
+    late String message;
+
+    if (plateCode == 1) {
+      message = "A placa deve conter 7 dígitos";
+    } else if (plateCode == 2) {
+      message = "A placa deve começar com 3 letras em sequencia";
+    } else if (plateCode == 3) {
+      message = "A placa deve terminar com 4 numeros em sequencia";
+    }
+
+    return message;
+  }
+
+  int verifyId(String id) {
+    const cpfLen = 11;
+    const cnpjLen = 14;
+    if ((id.length != cpfLen) && (id.length != cnpjLen)) {
+      return 1; // Não é nem CNPJ nem CPF por falta de numeros
+    } else if (id.length == cpfLen) {
+      for (int i = 0; i < (cpfLen); i++) {
+        if (!id[i].contains(new RegExp(r'[0-9]'))) {
+          return 2; // Não é CPF porque CPF deve conter apenas números
+        }
+      }
+    } else if (id.length == cnpjLen) {
+      for (int i = 0; i < (cnpjLen); i++) {
+        if (!id[i].contains(new RegExp(r'[0-9]'))) {
+          return 3; // Não é CNPJ porque CNPJ deve conter apenas números
+        }
+      }
+    }
+    return 0;
+  }
+
+  String getIdMessage(int idCode) {
+    late String message;
+    if (idCode == 1) {
+      message = "Não possui digitos suficientes";
+    } else if (idCode == 2) {
+      message = "CPF deve conter apenas números";
+    } else if (idCode == 3) {
+      message = "CNPJ deve conter apenas números";
+    }
+
+    return message;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -44,26 +136,92 @@ class bottomContainerComprarTicket extends StatelessWidget {
       width: MediaQuery.of(context).size.width,
       alignment: Alignment.center,
       child: Padding(
-        padding: const EdgeInsets.fromLTRB(0, 40, 0, 0),
+        padding: const EdgeInsets.fromLTRB(50, 50, 50, 50),
+        // padding: const EdgeInsets.fromLTRB(0, 40, 0, 0),
         child: Column(
-          children: const [
-            CustomTextfield(
-                text: "Placa do Veículo",
-                icon: Icon(
-                  Icons.directions_car_outlined,
-                  color: Color(0xFF3F3F3F),
-                )),
-            CustomTextfield(
-                text: "CPF/CNPJ",
-                icon: Icon(
-                  Icons.person_outline,
-                  color: Color(0xFF3F3F3F),
-                )),
-            CustomButton(
-              text: "Próximo",
-              screen: SelecionarTempo(),
-              validation: 1,
+          children: <Widget>[
+            TextField(
+              controller: _plate,
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.directions_car),
+                filled: true,
+                fillColor: Colors.white54,
+                border: OutlineInputBorder(),
+                labelText: 'Placa',
+                hintText: 'xxx 0000',
+                errorText: _validatePlate
+                    ? getPlateMessage(verifyPlate(_plate.text.toString()))
+                    : null,
+              ),
             ),
+            SizedBox(
+              height: 30,
+            ),
+            TextField(
+              controller: _id,
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.person),
+                filled: true,
+                fillColor: Colors.white54,
+                border: OutlineInputBorder(),
+                labelText: 'CPF/CNPJ',
+                hintText: 'xxx xxx xxx xx',
+                errorText: _validateId
+                    ? getIdMessage(verifyId(_id.text.toString()))
+                    : null,
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Container(
+                width: 217,
+                height: 47,
+                decoration: BoxDecoration(
+                  color: Color(0xFF505050),
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(30),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.25),
+                      spreadRadius: 1,
+                      blurRadius: 10,
+                      offset: const Offset(0, 4),
+                    )
+                  ],
+                ),
+                child: FlatButton(
+                  onPressed: () {
+                    setState(() {
+                      verifyId(_id.text.toString()) != 0
+                          ? _validateId = true
+                          : _validateId = false;
+                      verifyPlate(_plate.text.toString()) != 0
+                          ? _validatePlate = true
+                          : _validatePlate = false;
+                    });
+                    if (!_validateId && !_validatePlate) {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => SelecionarTempo()),
+                      );
+                    }
+                  },
+                  child: Text(
+                    "Proximo",
+                    style: const TextStyle(
+                      fontFamily: 'RobotoBold',
+                      fontSize: 16,
+                      color: Color(0xFFFFFFFF),
+                    ),
+                  ),
+                ),
+              ),
+            )
           ],
         ),
       ),
@@ -71,6 +229,13 @@ class bottomContainerComprarTicket extends StatelessWidget {
   }
 }
 
+class CidadaoInfo {
+  String id;
+  int plate;
+  CidadaoInfo(this.id, this.plate);
+}
+
+/*
 class CustomTextfield extends StatelessWidget {
   final String text;
   final Icon icon;
@@ -124,3 +289,4 @@ class CustomTextfield extends StatelessWidget {
     );
   }
 }
+*/
